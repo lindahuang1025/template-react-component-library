406d3825ebd501031319732be8927341c16e77b7
LindaHuangzixia <hzx1625759939@gmail.com>
2023-08-09 15:24:26 +0800

    add storybook

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,7 @@
-export default function(){
-    return {
-        presets: [
-          "@babel/preset-env",
-          "@babel/preset-react",
-          "@babel/preset-typescript",
-        ],
-    };
+export default {
+    presets: [
+      "@babel/preset-env",
+      "@babel/preset-react",
+      "@babel/preset-typescript",
+    ],
 }
